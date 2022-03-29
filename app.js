3be2775854261e304bb24f60e45fe3c234bee4e6
Student <owen.kiteck@stu.barren.kyschools.us>
2022-03-29 11:53:20 -0500

    finished bonus

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ const fourth = document.querySelector('#fourth');
 const p7 = document.createElement('p');
 fourth.append(p7);
 
-async function example4(){
+async function tvMazeFunc(){
     try {
         const show = await axios.get(`https://api.tvmaze.com/shows/38963/episodebynumber?season=2&number=8`);
         console.log(show);
@@ -64,7 +64,34 @@ async function example4(){
     } catch (err) {
         console.log(err);
         p7.innerText = err
-
     }
 }
-example4();
+tvMazeFunc();
+
+
+// BONUS
+// 5
+const body = document.querySelector(`body`)
+const img1 = document.createElement(`img`)
+body.append(img1)
+
+async function pokemonFunc(){
+    const pikachu = await axios.get(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+    img1.src = pikachu.data.sprites.front_female;
+}
+pokemonFunc();
+
+
+// 6
+const img2 = document.createElement(`img`)
+body.append(img2)
+
+async function omdb(){
+    const request = await axios.get(`http://www.omdbapi.com/?apikey=7e1fedee&t=frozen`)
+    img2.src = request.data;
+    console.log(request);
+    img2.src = request.data.Poster;
+}
+omdb();
+
+// 7e1fedee
